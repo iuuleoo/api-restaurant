0d7db1ede33daf5a85ddeea81ca58b4a537cb0f9
Leo <leooo0s2@icloud.com>
2026-01-13 15:00:56 -0300

    Validando dados do produto

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "@/utils/AppError";
+import { ZodError, z } from "zod";
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       return response.json({ message: "List of products" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, { message: "Value must be greater than zero" }),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      return response.status(201).json({ name, price });
     } catch (error) {
       next(error);
     }
