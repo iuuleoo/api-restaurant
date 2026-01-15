931a6c095519444a5440724ced31eec92ec195f7
Leo <leooo0s2@icloud.com>
2026-01-14 22:08:44 -0300

    Tratando Dados do Pedido.

--- src/database/migrations/20260115004127_create-orders.ts
+++ src/database/migrations/20260115004127_create-orders.ts
@@ -2,21 +2,28 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("orders", (table) => {
-    table.increments("id").primary(),
-      table
-        .integer("table_session_id")
-        .notNullable()
-        .references("id")
-        .inTable("tables_sessions"),
-      table
-        .integer("product_id")
-        .notNullable()
-        .references("id")
-        .inTable("products"),
-      table.integer("quantity").notNullable(),
-      table.decimal("price").notNullable(),
-      table.timestamp("created_at").defaultTo(knex.fn.now()),
-      table.timestamp("updated_at").defaultTo(knex.fn.now());
+    table.increments("id").primary();
+
+    table
+      .integer("table_session_id")
+      .notNullable()
+      .references("id")
+      .inTable("tables_sessions")
+      .onDelete("CASCADE");
+
+    table
+      .integer("product_id")
+      .notNullable()
+      .references("id")
+      .inTable("products")
+      .onDelete("CASCADE");
+
+    table.integer("quantity").notNullable();
+
+    table.decimal("price", 10, 2).notNullable();
+
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
 }
 
