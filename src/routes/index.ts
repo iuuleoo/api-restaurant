a60f6e8d771f65d0f543cba3a86a43e087661376
Leo <leooo0s2@icloud.com>
2026-01-14 21:07:29 -0300

    Rota e Controller de sessão da mesa

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { tablesSessionsRoutes } from "./tables-sessions-routes";
 import { productsRoutes } from "./products-routes";
 import { tablesRoutes } from "./tables-routes";
 
@@ -7,5 +8,6 @@ const routes = Router();
 
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRoutes);
+routes.use("/tables-sessions", tablesSessionsRoutes);
 
 export { routes };
